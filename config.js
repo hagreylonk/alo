7b3a511610dbdd3a5191bee7730b9e088590b8d9
hagreylonk <86775837+hagreylonk@users.noreply.github.com>
2021-07-01 17:08:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Xin chào bạn Như Phương!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao bạn cứ đi nhầm đường
+    Lạc vào tim tui lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Như Phương lại quanh quẩn đầu tui <3',
+    btnReply: 'Gửi cho tui <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'tui biết mà 🥰. tui cũng .... bà nhiều nhiều 😘😘',
+    messDesc: 'Tối nay tui qua đón đi chơi nha,à mà quên bà còn coi phimmmm',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: https://www.facebook.com/profile.php?id=100012466810759
